4f91259dde4ebbeafc61ea0b403450d94c4b5148
Pim Zonneveld <pimzonneveld@gmail.com>
2018-02-22 16:32:58 +0100

    Backend for mr cocktail working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'doses/destroy'
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
+
+  # resources :comments, only: [:index, :new, :create]
 end
